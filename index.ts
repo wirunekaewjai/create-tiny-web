b637b6fc74dd680f1d4c97a930d5ee4ace08e0bd
Wirune Kaewjai <wirune.kae@hotmail.com>
2024-05-26 10:29:02 +0700

    update script

--- index.ts
+++ index.ts
@@ -4,15 +4,14 @@ import path from "node:path";
 
 const src = path.join(__dirname, "template");
 const dst = process.argv[2];
+const name = process.argv[3];
 
 if (!dst) {
   throw "Please provide destination";
 }
 
-const projectName = path.dirname(path.join(dst, "test.ts")).split("/").pop()?.toLowerCase();
-
-if (!projectName) {
-  throw "Unable to generate project name";
+if (!name) {
+  throw "Please provide project name";
 }
 
 await cp(src, dst, {
@@ -30,9 +29,9 @@ for (const file of files) {
   const filePath = path.join(dst, file);
 
   const text = await readFile(filePath, "utf8");
-  const textOut = text.replaceAll("PROJECT_NAME", projectName);
+  const textOut = text.replaceAll("PROJECT_NAME", name);
 
   await writeFile(filePath, textOut, "utf8");
 }
 
-console.log(`project ${projectName} created`);
+console.log(`project ${name} created`);
