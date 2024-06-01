5f14a84a94deab484a05a6a9e55ecd344f384349
Wirune Kaewjai <wirune.kae@hotmail.com>
2024-06-02 01:20:01 +0700

    update jetpack

--- template/src/client/app.ts
+++ template/src/client/app.ts
@@ -1,8 +1,8 @@
 import { counter } from "@/client/views/counter";
-import { hxGet } from "@wirunekaewjai/jetpack";
+import { onHxGet } from "@wirunekaewjai/jetpack";
 
 // +/- on client-side
-hxGet((path, query) => {
+onHxGet((path, query) => {
   if (path === "/@counter") {
     const count = Number(query.get("count"));
     return counter(count);
