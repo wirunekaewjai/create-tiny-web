6850ec74e546c2867b7bf312dd24c63a6c99d21b
Wirune Kaewjai <wirune.kae@hotmail.com>
2024-06-02 21:36:17 +0700

    update jetpack

--- template/src/client/app.ts
+++ template/src/client/app.ts
@@ -1,9 +1,8 @@
 import { counter } from "@/client/views/counter";
 import { hxResponse } from "@wirunekaewjai/jetpack";
 
-htmx.on("htmx:afterSwap", (e) => {
-  const event = e as CustomEvent;
-  const detail = event.detail as {
+const onAfterSwap = (e: CustomEvent) => {
+  const detail = e.detail as {
     elt: HTMLElement;
     pathInfo: {
       requestPath: string;
@@ -16,11 +15,11 @@ htmx.on("htmx:afterSwap", (e) => {
   if (pathname.startsWith("/icons/")) {
     return detail.elt.setAttribute("name", pathname.split("/")[2]);
   }
-});
+};
 
-htmx.on("htmx:beforeRequest", (e) => {
-  const event = e as CustomEvent;
-  const conf = event.detail.requestConfig as {
+const onBeforeRequest = (e: CustomEvent) => {
+  const xhr = e.detail.xhr as XMLHttpRequest;
+  const conf = e.detail.requestConfig as {
     path: string;
     parameters: Record<string, any>;
   };
@@ -28,15 +27,26 @@ htmx.on("htmx:beforeRequest", (e) => {
   const [pathname, search] = conf.path.split("?");
 
   if (pathname === "/@count") {
+    e.preventDefault();
+
     const hxVals = conf.parameters;
     const query = new URLSearchParams(search);
 
     const name = query.get("name") ?? hxVals.name;
     const value = Number(query.get("value") ?? hxVals.value);
 
-    return hxResponse(event, {
+    return hxResponse(xhr, {
       body: counter(name, value),
       url: conf.path,
     });
   }
+};
+
+htmx.on("htmx:afterSwap", (e) => {
+  return onAfterSwap(e as CustomEvent);
+});
+
+// wrapped async function with normal function because htmx not supported async function
+htmx.on("htmx:beforeRequest", (e) => {
+  return onBeforeRequest(e as CustomEvent);
 });
