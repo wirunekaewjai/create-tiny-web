8c9a56563254c0caeccfe21d5f9d57ec6dcc281a
Wirune Kaewjai <wirune.kae@hotmail.com>
2024-06-03 14:32:36 +0700

    add --sync-jetpack

--- index.ts
+++ index.ts
@@ -1,38 +1,56 @@
 #!/usr/bin/env bun
-import { cp, readFile, writeFile } from "node:fs/promises";
+import { cp, readFile, rm, writeFile } from "node:fs/promises";
 import path from "node:path";
 
 const src = path.join(__dirname, "template");
 const dst = process.argv[2];
-const name = process.argv[3];
+const nameOrCommand = process.argv[3];
 
 if (!dst) {
   throw "Please provide destination";
 }
 
-if (!name) {
-  throw "Please provide project name";
+if (!nameOrCommand) {
+  throw "Please provide project name or command";
 }
 
-await cp(src, dst, {
-  force: true,
-  preserveTimestamps: true,
-  recursive: true,
-});
+if (nameOrCommand === "--sync-jetpack") {
+  const jetpackSrc = path.join(src, "jetpack");
+  const jetpackDst = path.join(dst, "jetpack");
 
-const files = [
-  "Cargo.toml",
-  "Dockerfile",
-  "package.json",
-];
+  await rm(jetpackDst, {
+    force: true,
+    recursive: true,
+  });
 
-for (const file of files) {
-  const filePath = path.join(dst, file);
-
-  const text = await readFile(filePath, "utf8");
-  const textOut = text.replaceAll("PROJECT_NAME", name);
-
-  await writeFile(filePath, textOut, "utf8");
+  await cp(jetpackSrc, jetpackDst, {
+    force: true,
+    preserveTimestamps: true,
+    recursive: true,
+  });
 }
 
-console.log(`project ${name} created`);
+else {
+  await cp(src, dst, {
+    force: true,
+    preserveTimestamps: true,
+    recursive: true,
+  });
+
+  const files = [
+    "Cargo.toml",
+    "Dockerfile",
+    "package.json",
+  ];
+
+  for (const file of files) {
+    const filePath = path.join(dst, file);
+
+    const text = await readFile(filePath, "utf8");
+    const textOut = text.replaceAll("PROJECT_NAME", nameOrCommand);
+
+    await writeFile(filePath, textOut, "utf8");
+  }
+
+  console.log(`project ${nameOrCommand} created`);
+}
